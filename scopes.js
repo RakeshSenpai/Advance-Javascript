05d315deec06dd2b77802e597fbcff88c511566f
Rakesh07778777 <bossrakesh43@gmail.com>
2026-09-07 23:07:27 +0530

    scopes

---
+++ scopes.js
@@ -0,0 +1,28 @@
+//Global Scopes
+
+// const userName = 'Rakesh';
+// function fun1(){
+//     console.log(userName);
+// }
+
+// fun1()
+
+
+// =============++=======
+
+//Functional scopes --> A variable declared inside a function is accessible inside that function, but not outside.
+
+// function calculatePrice(price, tax){
+//     const amount = price * tax
+//     const total = price + amount
+
+//     console.log(total)
+// }
+
+
+// calculatePrice(240, 12)
+
+
+// ========++++
+
+// Block Scope
