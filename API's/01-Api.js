707df7ff57f5efb0446b70d8de786c1783058370
Rakesh07778777 <bossrakesh43@gmail.com>
2026-09-14 23:50:05 +0530

    API

--- API's/01-Api.js
+++ API's/01-Api.js
@@ -1,2 +1,8 @@
-const button = document.getElementById('search-btn')
-const inputElm = document.getElementById('input-box')
+const button = document.getElementById('search-btn');
+const inputElm = document.getElementById('input-box');
+
+async function getData(city) {
+    const ApiUrl = await fetch (`http://api.weatherapi.com/v1/current.json?key=69f08d16f2024ac3b7e180601261409&q=${city}&aqi=yes`);
+    
+    
+}
