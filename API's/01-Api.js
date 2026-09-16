998f570aa55bea580b5339a12ebb21f2f83c1d86
Rakesh07778777 <bossrakesh43@gmail.com>
2026-09-16 23:25:10 +0530

    api

--- API's/01-Api.js
+++ API's/01-Api.js
@@ -2,7 +2,13 @@ const button = document.getElementById('search-btn');
 const inputElm = document.getElementById('input-box');
 
 async function getData(city) {
-    const ApiUrl = await fetch (`http://api.weatherapi.com/v1/current.json?key=69f08d16f2024ac3b7e180601261409&q=${city}&aqi=yes`);
+    const apiUrl = await fetch (`http://api.weatherapi.com/v1/current.json?key=69f08d16f2024ac3b7e180601261409&q=${city}&aqi=yes`);
     
-    
+    return await apiUrl.json();
 }
+
+button.addEventListener('click', async () => {
+    const value = inputElm.value;
+    const result = await getData(value); 
+    console.log(result);
+})
