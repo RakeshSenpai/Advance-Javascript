7cbf084da6f71c4103bfa5c167e4509086245397
Rakesh07778777 <bossrakesh43@gmail.com>
2026-09-17 23:54:29 +0530

    all set api

--- API's/01-Api.js
+++ API's/01-Api.js
@@ -14,6 +14,8 @@ async function getData(city) {
 button.addEventListener('click', async () => {
     const value = inputElm.value;
     const result = await getData(value);
-    console.log(result)
     cityName.innerText = `${result.location.name}, ${result.location.region}, ${result.location.country}`
+    cityTime.innerText = `${result.location.localtime}`
+    cityTemp.innerText = `${result.current.temp_c}`
+    console.log(result)
 }) 
