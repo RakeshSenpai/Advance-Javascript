be8803dc1eab59e0c86e9f482d0a8b7c66fbd7cf
Rakesh07778777 <bossrakesh43@gmail.com>
2026-09-17 23:49:54 +0530

    api weather

--- API's/01-Api.js
+++ API's/01-Api.js
@@ -1,6 +1,10 @@
 const button = document.getElementById('search-btn');
 const inputElm = document.getElementById('input-box');
 
+const cityName = document.getElementById('city-name')
+const cityTime = document.getElementById('city-time')
+const cityTemp = document.getElementById('city-temp')
+
 async function getData(city) {
     const apiUrl = await fetch (`http://api.weatherapi.com/v1/current.json?key=69f08d16f2024ac3b7e180601261409&q=${city}&aqi=yes`);
     
@@ -9,6 +13,7 @@ async function getData(city) {
 
 button.addEventListener('click', async () => {
     const value = inputElm.value;
-    const result = await getData(value); 
-    console.log(result);
-})
+    const result = await getData(value);
+    console.log(result)
+    cityName.innerText = `${result.location.name}, ${result.location.region}, ${result.location.country}`
+}) 
